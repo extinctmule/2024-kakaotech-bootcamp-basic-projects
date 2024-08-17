fe42b7e7712163107aa28d6bf92f8bc6e9f59fb8
extinctmule <extinctmule@gmail.com>
2024-08-17 20:48:24 +0900

    Update: refactor html files using jQuery

--- web/colors.js
+++ web/colors.js
@@ -1,21 +1,27 @@
 var Body = {
   setColor: function (color) {
-    document.querySelector("body").style.color = color;
+    $("body").css("color", color);
+    //document.querySelector("body").style.color = color;
   },
   setBackgroundColor: function (color) {
-    document.querySelector("body").style.backgroundColor = color;
+    $("body").css("backgroundColor", color);
+    //document.querySelector("body").style.backgroundColor = color;
   },
 };
 
 var Links = {
   setColor: function (color) {
-    var alist = document.querySelectorAll("a");
-    var i = 0;
-    while (i < alist.length) {
-      alist[i].style.color = color;
-      i = i + 1;
-    }
+    //이 웹 페이지에 있는 모든 a태그를 jQuery로 제어하겠단 뜻.
+    $("a").css("color", color);
   },
+  // setColor: function (color) {
+  //   var alist = document.querySelectorAll("a");
+  //   var i = 0;
+  //   while (i < alist.length) {
+  //     alist[i].style.color = color;
+  //     i = i + 1;
+  //   }
+  // },
 };
 
 function setColor(color) {
